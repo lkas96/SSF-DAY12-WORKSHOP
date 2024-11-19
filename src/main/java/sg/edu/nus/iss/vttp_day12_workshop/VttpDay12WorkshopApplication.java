ba95b1f5f0976e0d9f9e4077bc0489d881c41231
Lawson Kwek <63045323+lkas96@users.noreply.github.com>
2024-11-19 20:03:00 +0800

    Springboot Initial Template

---
+++ src/main/java/sg/edu/nus/iss/vttp_day12_workshop/VttpDay12WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day12_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay12WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay12WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vttp_day12_workshop/VttpDay12WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vttp_day12_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay12WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
